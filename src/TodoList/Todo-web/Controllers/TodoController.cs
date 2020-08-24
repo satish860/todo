53299f500c701c8cebed29de026058426d879702
Satish Venkatakrishnan <svenkatakris@vmware.com>
2020-08-24 12:03:07 +0530

    Initial Checkin for TodoList
    This contains Inmemory version. Have to work on adding Postgres

---
+++ src/TodoList/Todo-web/Controllers/TodoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Todo_web.Models;
+
+namespace Todo_web.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<TodoModel> todoModels = new List<TodoModel>
+            {
+                new TodoModel{ToDoId=1,Item="Hello World",DateAdded=DateTime.Now},
+                new TodoModel{ToDoId=2,Item="Something says me that we have a lot to do in this world",DateAdded=DateTime.Now}
+            };
+            return View(todoModels);
+        }
+    }
+}
---
+++ src/TodoList/Todo-web/Models/TodoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo_web.Models
+{
+    public class TodoModel
+    {
+        
+        public int ToDoId { get; set; }
+
+        public string Item { get; set; }
+
+        public DateTime DateAdded { get; set; }
+    }
+}
